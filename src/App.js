2ca6f0379ff743a71f577b967d99e8a19facb683
Aritra Deb <aritradeb45@gmail.com>
2024-10-03 15:37:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import { AuthProvider } from './AuthContext';
 import Registration from './Registration';
 import Login from './Login';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import Home from './Home';
 import YourProducts from './YourProducts';
 import Profile from './Profile';
@@ -11,13 +10,17 @@ import ProductCard from './ProductCard';
 import AboutUs from './AboutUs';
 import YourInterest from './YourInterest';
 import ProtectedRoute from './ProtectedRoute';
+import LandingPage from './LandingPage';  // Import the LandingPage component
 
 const App = () => {
     return (
         <AuthProvider>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Navigate to="/login" />} />
+                    {/* Route to the landing page at the root URL */}
+                    <Route path="/" element={<LandingPage />} />
+
+                    {/* Other existing routes */}
                     <Route path="/register" element={<Registration />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/home" element={<ProtectedRoute element={Home} />} />
@@ -26,7 +29,8 @@ const App = () => {
                     <Route path="/productCard" element={<ProtectedRoute element={ProductCard} />} />
                     <Route path="/about" element={<ProtectedRoute element={AboutUs} />} />
                     <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
-                    
+
+                    {/* Default redirect to /home */}
                     <Route path="*" element={<Navigate to="/home" />} />
                 </Routes>
             </Router>
